098ac6da7a6d89c6b350c04c1724d99556af7839
Keir Innes <keir.innes@gmail.com>
2021-08-09 17:01:33 +1200

    adding functionality

--- js-code.js
+++ js-code.js
@@ -1,7 +1,8 @@
 const mainCanvas = document.querySelector('div[data-key="mainCanvas"]');
 const boardSize = 600;
 let rowsColumns = 16;
-let mouseDown = 0;
+let mouseClicked = 0;
+
 
 drawBoard(rowsColumns);
 mainCanvas.style.gridTemplateRows = `repeat(${rowsColumns}, ${boardSize / rowsColumns}px)`;
@@ -18,16 +19,28 @@ function drawBoard(number) {
     mainCanvas.innerHTML = "";
     for (let i = 1; i <= number * number; i++) {
         let tempDiv = document.createElement('div');
-        tempDiv.style.border = '.2px solid lightgray'
-        tempDiv.addEventListener('mouseenter', colorChangeFunc)
-        tempDiv.addEventListener('mousedown', () => { mouseDown = 1 });
-        tempDiv.addEventListener('mouseup', () => { mouseDown = 0 });
+        tempDiv.style.border = '.2px solid lightgray';
+        tempDiv.addEventListener('mouseenter', colorChangeHover);
+        tempDiv.addEventListener('mousedown', colorChangeClick);
+        tempDiv.addEventListener('mouseup', mouseUp);
+        tempDiv.setAttribute('draggable', 'false');        
         mainCanvas.appendChild(tempDiv);
     }
 }
-function colorChangeFunc(e) {
-    if (mouseDown === 1) {
+function colorChangeClick(e) {
+    mouseClicked = 1;
+    e.target.style.backgroundColor = 'lightGray';
 
+}
+
+function mouseUp() {
+    mouseClicked = 0;
+}
+function colorChangeHover(e) {
+    //if (e.buttons > 0) {
+    if(mouseClicked == 1) {
+        //e.stopPropogation();
+        e.preventDefault();
         e.target.style.backgroundColor = 'lightGray';
     }
 }
@@ -44,7 +57,8 @@ document.body.onmousedown = function() {
     ++mouseDown;
     console.log(mouseDown)
   }
-  */
+
 document.body.onmouseup = function () {
     mouseDown = 0;
 }
+*/
